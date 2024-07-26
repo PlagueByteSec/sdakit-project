1f3bffb82a1a6f629f33417231375ab5f777e97e
fhAnso <felix.hansow@proton.me>
2024-07-26 21:30:20 +0200

    Moved arg in banner and file ext check added

--- lib/output.go
+++ lib/output.go
@@ -2,9 +2,14 @@ package lib
 
 import (
 	"os"
+	"strings"
 )
 
 func WriteOutput(filePath string, content string) {
+	hasExt := strings.HasSuffix(filePath, ".txt")
+	if !hasExt {
+		filePath = filePath + ".txt"
+	}
 	stream, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		GetPanic("ERROR: %s\n", err)
