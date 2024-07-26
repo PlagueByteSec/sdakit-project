6cd04e2786b955b71b17077fae38ba241a7fec65
fhAnso <felix.hansow@proton.me>
2024-07-26 17:29:04 +0200

    Unnecessary brackets removed

--- lib/utils.go
+++ lib/utils.go
@@ -5,11 +5,11 @@ type Pool map[string]string
 
 // Add new entry to result pool
 func (pool Pool) AddEntry(entry string) {
-	(pool)[entry] = entry
+	pool[entry] = entry
 }
 
 // Verify that the entry does not exist in Pool
 func (pool Pool) ContainsEntry(entry string) bool {
-	_, exists := (pool)[entry]
+	_, exists := pool[entry]
 	return exists
 }
