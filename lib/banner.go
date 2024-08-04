41a4a8780b6625214369018558af7a37926468eb
fhAnso <felix.hansow@proton.me>
2024-08-04 18:52:04 +0200

    Help banner adjusted

--- lib/banner.go
+++ lib/banner.go
@@ -5,20 +5,21 @@ Sentinel
 ********
 ===[ Description
  This program is designed to fetch, filter and validate subdomains from a specific host.
- The Sentinel project replaces the platform-dependent script "uma.sh" and makes it possible
- to passively enumerate subdomains of a target independently of the OS. The results will be 
- saved among each other to provide a quick solution for further processing.
+ Subdomains can be enumerated by requesting external ressources (passive) or via brute 
+ force (direct). 
+
+ Sentinel is published under the MIT license: https://github.com/fhAnso/Sentinel
 
 ===[ Usage
- -t Set the target hostname. 
-	[passive] (Without -w) this hostname will be be send to "RapidDNS" and "crt.sh"
-	Example: targethostname.xyz 
- -o Specify the output (.txt) file where every regex match from the main pool will be saved
+ -t Set the target domain name. 
+	[passive] (Without -w) request subdomains from external endpoints
+	Example: targetdomain.xyz 
+ -o Specify the output (.txt) file 
  -c Send GET request to retrieve the HTTP status code of every passive enumeration result
  -w [direct] Use wordlist to bruteforce subdomains of the target
  -e Exclude response codes from bruteforce results (comma seperated)
  -s Only show subdomains that can be resolved to ip addresses 
- 
+
 ===[ Not implemented yet
  -f Show only specific HTTP response codes from bruteforce results (comma seperated)
 `
