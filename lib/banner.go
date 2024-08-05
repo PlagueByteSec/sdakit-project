f6bb16a883ce4fd5ac50b78ec7ee92d2895f0abc
fhAnso <felix.hansow@proton.me>
2024-08-05 18:36:25 +0200

    Help banner adjusted

--- lib/banner.go
+++ lib/banner.go
@@ -11,16 +11,18 @@ Sentinel
  Sentinel is published under the MIT license: https://github.com/fhAnso/Sentinel
 
 ===[ Usage
- -v Verbose output
- -t Set the target domain name. 
-	[passive] (Without -w) request subdomains from external endpoints
-	Example: targetdomain.xyz 
- -o Specify the output (.txt) file 
- -c Send GET request to retrieve the HTTP status code of every passive enumeration result
- -w [direct] Use wordlist to bruteforce subdomains of the target
- -e Exclude response codes from bruteforce results (comma seperated)
- -s Only show subdomains that can be resolved to ip addresses 
+ -v  Verbose output
+ -t  Set the target domain name. 
+	 [passive] (Without -w) request subdomains from external endpoints
+	 Example: targetdomain.xyz 
+ -o  Specify the output (.txt) file for subdomains
+ -v4 Specify the output (.txt) file for IPv4 addresses
+ -v6 Specify the output (.txt) file for IPv6 addresses
+ -c  Send GET request to retrieve the HTTP status code of every passive enumeration result
+ -w  [direct] Use wordlist to bruteforce subdomains of the target
+ -e  Exclude response codes from bruteforce results (comma seperated)
+ -s  Only show subdomains that can be resolved to ip addresses 
 
 ===[ Not implemented yet
- -f Show only specific HTTP response codes from bruteforce results (comma seperated)
+ -f  Show only specific HTTP response codes from bruteforce results (comma seperated)
 `
