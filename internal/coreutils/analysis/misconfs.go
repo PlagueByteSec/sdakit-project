a42b31f8441e2f8e6b136371367481d380a96020
fhAnso <fh4nso@tuta.io>
2024-09-09 13:20:30 +0200

    bugfix: cookieInjectionPath and add build scripts

--- internal/coreutils/analysis/misconfs.go
+++ internal/coreutils/analysis/misconfs.go
@@ -13,8 +13,7 @@ func (check *SubdomainCheck) cookieInjectionPath() {
 	// session hijacking, xss
 	testHeader := "Set-Cookie"
 	testCookie := "jzqvtyxkplra"
-	url := makeUrl(HTTP(Secure), check.Subdomain) +
-		"%0d%0a" + fmt.Sprintf("%s:+tookie=%s", testHeader, testCookie)
+	url := makeUrl(HTTP(Secure), check.Subdomain) + "%0d%0a" + testHeader + "%3A" + fmt.Sprintf("+tookie=%s", testCookie)
 	if check.isPayloadReflected(url, HeadersCompare{
 		TestHeaderKey:   testHeader,
 		TestHeaderValue: testCookie,
